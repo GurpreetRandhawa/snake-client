202f70c845b03cb716d0ad993a92df0b7881daf9
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-24 15:53:48 -0800

    export destructure

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: "50541",
+  });
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  conn.setEncoding("utf-8");
+
+  return conn;
+};
+module.exports = { connect };
